823817a447dc000e9deff9a7523ae720889e9ca8
johan osorio hernandez <osojohan222@gmail.com>
2024-08-30 00:13:16 -0500

    primera version

---
+++ js/alert.js
@@ -0,0 +1,36 @@
+Swal.fire({
+    title: "ingrese su nombre",
+    input: "text",
+    inputAttributes: {
+      autocapitalize: "off"
+    },
+    showCancelButton: true,
+    confirmButtonText: "entrar",
+    showLoaderOnConfirm: true,
+    preConfirm: async (login) => {
+      try {
+        const githubUrl = `
+          https://api.github.com/users/${login}
+        `;
+        const response = await fetch(githubUrl);
+        if (!response.ok) {
+          return Swal.showValidationMessage(`
+            ${JSON.stringify(await response.json())}
+          `);
+        }
+        return response.json();
+      } catch (error) {
+        Swal.showValidationMessage(`
+          Request failed: ${error}
+        `);
+      }
+    },
+    allowOutsideClick: () => !Swal.isLoading()
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: `${result.value.login}'s avatar`,
+        imageUrl: result.value.avatar_url
+      });
+    }
+  });
